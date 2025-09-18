bad88ffa3d56d70006085c22a51b47ba30923b6b
Kalyani wasane <144616357+hgeydkalyani@users.noreply.github.com>
2025-09-18 14:37:07 +0530

    Update Employee.java
    in which i add the database

--- Employee.java
+++ Employee.java
@@ -38,7 +38,41 @@ public class Employee  extends HttpServlet {
 		System.out.println(address);
 		System.out.println(city);
 		System.out.println(state);
+        try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection c =DriverManager .getConnection("jdbc:mysql://localhost:3306/demo1","root","Kalyani@123");
+			 PreparedStatement ps=c.prepareStatement("insert into employee1(name,id,age,email,phoneNo,gender,state,country,address,department,role, exp,sal)values (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+			 ps.setString(1,name);
+			 ps.setInt(2,id);
+			 ps.setInt(3,age);
+			 ps.setString(4,email);
+			 ps.setLong(5,phoneNo);
+			 ps.setString(6,gender);
+			 ps.setString(7,state);
+			 ps.setString(8,country);
+			 ps.setString(9,address);
+			 ps.setString(10,department);
+			 ps.setString(11,role);
+			 ps.setInt(12,exp);
+			 ps.setLong(13,sal);
+			 
+			 int check = ps.executeUpdate();
 
+				if (check > 0) {
+					out.println("<h1 style='color:green'>" + "Data is inserted..." + "</h1>");
+				} else {
+					out.print("<h1 style='backgroundcolor:red'>" + "Data is Not inserted..." + "</h1>");
+				}
+				c.close();
+
+			 
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			out.println(e.getMessage());
+		}
+        
+        
 
 }
 }
+
